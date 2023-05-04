b49426eda2a88dea26dbfd4fc926e84f88382d23
Rikriti <sweetrikriti0041@gmail.com>
2023-05-04 16:26:10 +0545

    some small changes

--- Home.js
+++ Home.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image,  StyleSheet } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+       <Image style={styles.image} source={require("./assets/graphimg.png")}  /> 
+       <Image style={styles.image} source={require("./assets/pie.png")}  /> 
+
     </View>
   )
 }
 
-export default Home
+const styles = StyleSheet.create({
+    image: {
+        margin: 20,
+        marginLeft:90,
+      },
+
+});
+
+export default Home;
